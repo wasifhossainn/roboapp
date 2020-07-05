021aaaec0faaf0dad17e7c96588d84c8bea8a7fe
Wasif Hossain <wasif.hossain@hotmail.com>
2020-07-05 19:41:16 +1000

    Updates

---
+++ precache-manifest.7f7754c6873fe85eb3aee04840ebd157.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8a85ceecd079d583a9094fb4b714c4e",
+    "url": "/roboapp/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/roboapp/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "05f1a97348a31e1a17be",
+    "url": "/roboapp/static/css/main.23f448a0.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/roboapp/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/roboapp/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05f1a97348a31e1a17be",
+    "url": "/roboapp/static/js/main.52fc108f.chunk.js"
+  },
+  {
+    "revision": "d659b12d7e33e29f7db9",
+    "url": "/roboapp/static/js/runtime-main.ebd44ea6.js"
+  }
+]);
